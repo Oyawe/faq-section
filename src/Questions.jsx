75e7f12efb4bda36ebf917930281fb97375bdd8d
Oyawe <oyawegoodness@gmail.com>
2023-08-28 21:23:17 -0700

    activeId stuff to toggle the questions

--- src/Questions.jsx
+++ src/Questions.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Question from "./Question";
 
-const Questions = ({ questions }) => {
+const Questions = ({ questions, activeId, toogleQuestion }) => {
   return (
     <section className="container">
       <h1>Questions</h1>
       {questions.map((question) => {
-        return <Question key={question.id} {...question} />;
+        return (
+          <Question
+            key={question.id}
+            {...question}
+            activeId={activeId}
+            toogleQuestion={toogleQuestion}
+          />
+        );
       })}
     </section>
   );
